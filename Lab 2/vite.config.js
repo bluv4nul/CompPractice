bbefda29473c7ecf55c79718fc050787e82f4485
Andrew <andrewkafrolov76@gmail.com>
2026-03-12 19:40:01 +0300

    try3

--- Lab 2/vite.config.js
+++ Lab 2/vite.config.js
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 
 export default {
-  base : '/CompPractice/',
+  base : '/CompPractice/Lab2/',
   root: resolve(__dirname, 'src'),
   build: {
     outDir: '../dist'
